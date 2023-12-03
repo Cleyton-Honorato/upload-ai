91b8130b126d078305808f7008978db0e880b84d
Cleyton Honorato <limacleyton82@gmail.com>
2023-12-03 11:20:43 -0300

    INIT

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,31 @@
+import { Github } from "lucide-react";
+
+import { Button } from "./components/ui/button";
+import { Separator } from "@radix-ui/react-separator";
+
 export function App() {
-  return <h1>Hello word!!</h1>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com 💙 por mim
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+
+      <main className="flex-1 p-6">
+        <h1>HELLO WORD!!</h1>
+      </main>
+    </div>
+  );
 }
